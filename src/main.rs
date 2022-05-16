a827959d4dc15098cd8c103bb2c095a64612b6e9
Creepsy <Robin.gd@outlook.de>
2022-05-16 21:50:19 +0200

    Simple rendering scene from bevy examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(startup)
+        .run();
+}
+
+fn startup(mut commands : Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color : Color::rgb(0.17, 0.44, 0.74),
+            custom_size: Some(Vec2::new(50.0, 50.05)),
+            ..default()
+        },
+        ..default()
+    });
 }
